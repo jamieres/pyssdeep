f02c8b89f20221b9e0953a40873277564f74a4d0
jose.monkey.org <jose.monkey.org@ff2fbd42-dc1f-11dd-a057-4b7f6b88b2f1>
2010-01-05 19:19:41 +0000

    more flexible tests

--- trunk/test.py
+++ trunk/test.py
@@ -1,15 +1,16 @@
-inputs = ('/home/jose/mcorral/bin/fotos.exe', 
-          '/home/jose/mcorral/bin/GoogleADwordscertSetup.exe',
-          '/home/jose/mcorral/bin/home.exe',
-          '/home/jose/mcorral/bin/MultyCodecUpgr.7.exe',
-          '/home/jose/mcorral/bin/My.YouTube.Movie.avi.exe',
-          '/home/jose/mcorral/bin/photo.exe',
-          '/home/jose/mcorral/bin/tt.exe',
-          '/home/jose/mcorral/bin/ups_invoice.exe')
+import os
+inputs = os.listdir('.')
 
 from ssdeep import ssdeep
 s = ssdeep()
+hashes = {}
 for i in inputs:
-    print '%s %20s' % (i, s.hash_file(i))
+    if i.startswith('.'): continue
+    h = s.hash_file(i)
+    print '%s %20s' % (i, h)
+    hashes[i] = h
 
-s.hash_file('/tmp/sowgweogewgew')
+print 'doing comparison'
+for k, v in hashes.iteritems():
+    for k2, v2 in hashes.iteritems():
+        print '%s <-> %s     %d' % (k, k2, s.compare(v, v2))
