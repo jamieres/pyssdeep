6b602b8b4c796317499d9095c5c0cc9473b92583
jose.monkey.org <jose.monkey.org@ff2fbd42-dc1f-11dd-a057-4b7f6b88b2f1>
2010-04-20 19:10:23 +0000

    add support for hash_bytes, issue #1, from flester

--- ssdeep_util.c
+++ ssdeep_util.c
@@ -12,20 +12,36 @@
 
 char *
 fuzzy_hash(char *filename) {
-	FILE *input;
-	int i;
-	char *hash, *res;
+        FILE *input;
+        int i;
+        char *hash;
 
-	hash = (char *)malloc(FUZZY_MAX_RESULT);
-	if (NULL == hash)
-		return("malloc oops");
-	input = fopen(filename, "rb");
-	if (NULL == input)
-		return("fopen oops");
-	i = fuzzy_hash_file(input, hash);
-	fclose(input);
-	if (0 == i) 
-		return(hash);
-	else
-		return("fuzzy_hash_file oops");
+        hash = (char *)malloc(FUZZY_MAX_RESULT);
+        if (NULL == hash)
+                return("malloc oops");
+        input = fopen(filename, "rb");
+        if (NULL == input)
+                return("fopen oops");
+        i = fuzzy_hash_file(input, hash);
+        fclose(input);
+        if (0 == i)
+                return(hash);
+        else
+                return("fuzzy_hash_file oops");
+}
+
+char *
+fuzzy_hash_data(char *buf, uint32_t buf_len) {
+        char *hash;
+        int i;
+
+        hash = (char *)malloc(FUZZY_MAX_RESULT);
+        if (NULL == hash)
+                return ("malloc oops");
+
+        i = fuzzy_hash_buf((unsigned char *)buf, buf_len,hash);
+        if (0 == i)
+                return (hash);
+        else
+                return ("fuzzy_hash_data oops");
 }
